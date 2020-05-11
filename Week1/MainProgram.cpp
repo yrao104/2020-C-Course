d4d599acb95c8dc1fceaf69e66dd1547ed87a6ed
Yamuna Rao <63515445+yrao104@users.noreply.github.com>
2020-05-10 22:33:52 -0700

    Create MainProgram.cpp

---
+++ Week1/MainProgram.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //helps print a console; iostream(library): short for input and output
+
+using namespace std; //standard namespace
+
+int main() //main function
+{
+    cout << "Hello, World!" << endl; //cout: console output; endl: end line ; print statement
+    return 0; //good convention to return a value
+}
