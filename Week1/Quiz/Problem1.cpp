b5eb997a6c88f5702c59433a046decee2a3bf27f
Yamuna Rao <63515445+yrao104@users.noreply.github.com>
2020-05-10 22:35:34 -0700

    Create Problem1.cpp

---
+++ Week1/Quiz/Problem1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float decimal = 32.5;
+    int integer = 80;
+    bool boolean = false;
+
+    cout << decimal << endl;
+    cout << integer << endl;
+    cout << boolean << endl;
+}
